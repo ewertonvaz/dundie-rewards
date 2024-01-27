5340ae8cc5b47560b0bab80d1bf944bd5594a262
ewertonvaz~ <ewerton.vaz@gmail.com>
2024-01-27 18:16:53 -0300

    criada a estrtura para o pacote dundie

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="dundie",
+    version="0.1.0",
+    description="Dunder-Miffin reward program",
+    author="Ewerton da Costa Vaz",
+    packages=find_packages()
+)
