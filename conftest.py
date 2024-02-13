ca0007d8400eafd84660ff21fc5b3a7d3b7a6f3a
ewertonvaz~ <ewerton.vaz@gmail.com>
2024-02-13 09:52:17 -0300

    ajustes para passar a pipeline

--- conftest.py
+++ conftest.py
@@ -1,7 +1,10 @@
 import pytest
+from unittest.mock import patch
 
-MARKER = """
+
+MARKER = """\
 unit: Mark unit tests
+integration: Mark integration tests
 high: High Priority
 medium: Medium Priority
 low: Low Priority
@@ -9,21 +12,23 @@ low: Low Priority
 
 
 def pytest_configure(config):
-    map(
-        lambda line: config.addinivalue_line("markers", line),
-        MARKER.split("\n"),
-    )
+    for line in MARKER.split("\n"):
+        config.addinivalue_line("markers", line)
 
 
 @pytest.fixture(autouse=True)
-def go_to_tmpdir(request):  # injeção de dependências
-    tmmpdir = request.getfixturevalue("tmpdir")
-    with tmmpdir.as_cwd():
-        yield
+def go_to_tmpdir(request):  # injeção de dependencias
+    tmpdir = request.getfixturevalue("tmpdir")
+    with tmpdir.as_cwd():
+        yield  # protocolo de generators
 
 
 @pytest.fixture(autouse=True, scope="function")
 def setup_testing_database(request):
-    """For each test
-
+    """For each test, create a database file on tmpdir
+    force database.py to use that filepath.
     """
+    tmpdir = request.getfixturevalue("tmpdir")
+    test_db = str(tmpdir.join("database.test.json"))
+    with patch("dundie.database.DATABASE_PATH", test_db):
+        yield
